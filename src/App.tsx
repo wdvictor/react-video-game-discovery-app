ea88bd981753dd1c76e89a2d322fde35a7329c2a
victot.antunes <victor.antunes@cade.gov.br>
2024-11-27 11:13:20 -0300

    Adding navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {  Grid, GridItem, Show, useBreakpointValue } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
 
 function App() {
 
@@ -15,7 +16,7 @@ function App() {
       base:`"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-    <GridItem area={'nav'} bg='coral'>Nav</GridItem>
+    <GridItem area={'nav'}><NavBar/></GridItem>
     <Show  when={isLargeScreen}>
     <GridItem area={'aside'} bg='gold'>Aside</GridItem>
 
