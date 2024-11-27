fab3e1f6c10401bc22447e6d55541ebe49a93b96
victot.antunes <victor.antunes@cade.gov.br>
2024-11-27 10:44:59 -0300

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from "@/components/ui/provider"
 import './index.css'
 import App from './App.tsx'
 
